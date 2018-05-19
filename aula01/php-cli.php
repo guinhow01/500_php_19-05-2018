a319403139401a231323f20d94f9fd7616ae9fe1
guinhow01 <38760757+guinhow01@users.noreply.github.com>
2018-05-19 17:59:15 -0300

    Add files via upload

--- aula01/php-cli.php
+++ aula01/php-cli.php
@@ -3,6 +3,13 @@
 //CLI - Command Line Interface
 // Interface de Linha de Comando
 
-$nome = readline('Digite seu nome: ');
-$idade = readline('Digite seu idade: ');
-echo "Olá, sou {$nome} e tenho {$idade} anos" . PHP_EOL;
+$nota = readline('Digite sua nota: ');
+$frequencia = readline('Digite sua frequencia: ');
+var_dump($nota);
+if ($nota >= 7 && $frequencia >= 8) {
+	echo "Aprovado" . PHP_EOL;
+}	else if ($nota >= 5 OR $nota < 7 && $frequencia >= 8) {
+	echo "Recuperacao" . PHP_EOL;
+} else {
+	echo "Reprovado" . PHP_EOL;
+}
